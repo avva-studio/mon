14099b0c867f8f221acb992ff09377580bbca84b
Glyn Owen Hanmer <glynhanmer@gmail.com>
2017-10-12 19:27:56 +0200

    Log when starting whether the DB is available or not.

--- Main.go
+++ Main.go
@@ -14,25 +14,36 @@ var connectionString string
 func main() {
 	router := NewRouter()
 	port := 8080
-	fmt.Printf("Starting GOHMoneyREST on port %d\n", port)
+	log.Printf("Starting GOHMoneyREST on port %d\n", port)
 	portString := fmt.Sprintf(`:%d`, port)
 	log.Fatal(http.ListenAndServe(portString, router))
 }
 
 func init() {
 	if len(os.Args) < 2 {
-		fmt.Println("No database connection file location given. Please provide the location of the connection string file as the first argument to the application.")
+		log.Println("No database connection file location given. Please provide the location of the connection string file as the first argument to the application.")
 		return
 	}
 	var err error
 	connectionString, err = GOHMoneyDB.LoadDBConnectionString(os.Args[1])
 	if err != nil {
-		fmt.Printf("Unable to load connection string from file at %s\n", os.Args[1])
+		log.Printf("Unable to load connection string from file at %s\n", os.Args[1])
 		return
 	}
-	router := NewRouter()
-	port := 8080
-	fmt.Printf("Starting GOHMoneyREST on port %d\n", port)
-	portString := fmt.Sprintf(`:%d`, port)
-	log.Fatal(http.ListenAndServe(portString, router))
+	logDBState()
+}
+
+func logDBState() {
+	db, err := GOHMoneyDB.OpenDBConnection(connectionString)
+	if err != nil {
+		log.Printf("Error opening database connection: %s", err)
+		return
+	}
+	defer db.Close()
+	msg := "Database is "
+	if !GOHMoneyDB.DbIsAvailable(db) {
+		msg += "not "
+	}
+	log.Print(msg + "available.")
+	return
 }
