7d2940e5a215958823e6670f358e793646e78ba7
Glyn Owen Hanmer <glynhanmer@gmail.com>
2018-04-02 23:48:25 +0200

    Move table writing to own package

--- cmd/cli/accounts.go
+++ cmd/cli/accounts.go
@@ -3,18 +3,13 @@ package main
 import (
 	"log"
 	"os"
-	"strconv"
 
 	"github.com/glynternet/accounting-rest/client"
-	"github.com/glynternet/go-accounting-storage"
-	"github.com/glynternet/go-time"
-	"github.com/olekukonko/tablewriter"
+	"github.com/glynternet/accounting-rest/pkg/table"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
-
-const dateFormat = `02-01-2006`
 
 var cmdAccounts = &cobra.Command{
 	Use: "accounts",
@@ -24,33 +19,10 @@ var cmdAccounts = &cobra.Command{
 		if err != nil {
 			log.Fatal(errors.Wrap(err, "selecting accounts"))
 		}
-		table(*as)
+		table.Accounts(*as, os.Stdout)
 	},
 }
 
 func init() {
 	cmdRoot.AddCommand(cmdAccounts)
-}
-
-func table(as storage.Accounts) {
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"ID", "Name", "Opened", "Closed", "Currency"})
-
-	for _, a := range as {
-		table.Append([]string{
-			strconv.FormatUint(uint64(a.ID), 10),
-			a.Name(),
-			a.Opened().Format(dateFormat),
-			closedString(a.Closed()),
-			a.CurrencyCode().String(),
-		})
-	}
-	table.Render() // Send output
-}
-
-func closedString(t time.NullTime) string {
-	if !t.Valid {
-		return ""
-	}
-	return t.Time.Format(dateFormat)
 }
---
+++ pkg/table/account.go
@@ -0,0 +1,35 @@
+package table
+
+import (
+	"io"
+	"strconv"
+
+	"github.com/glynternet/go-accounting-storage"
+	"github.com/glynternet/go-time"
+	"github.com/olekukonko/tablewriter"
+)
+
+const dateFormat = `02-01-2006`
+
+func Accounts(as storage.Accounts, w io.Writer) {
+	table := tablewriter.NewWriter(w)
+	table.SetHeader([]string{"ID", "Name", "Opened", "Closed", "Currency"})
+
+	for _, a := range as {
+		table.Append([]string{
+			strconv.FormatUint(uint64(a.ID), 10),
+			a.Name(),
+			a.Opened().Format(dateFormat),
+			closedString(a.Closed()),
+			a.CurrencyCode().String(),
+		})
+	}
+	table.Render() // Send output
+}
+
+func closedString(t time.NullTime) string {
+	if !t.Valid {
+		return ""
+	}
+	return t.Time.Format(dateFormat)
+}
