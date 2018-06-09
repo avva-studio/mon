68f96cb6adadeca3bf8bfc6aa5f10939df16a943
Glyn Owen Hanmer <glynhanmer@gmail.com>
2018-06-09 11:59:54 +0200

    deleteaccount: implement account handler

--- server/accounthandlers.go
+++ server/accounthandlers.go
@@ -108,6 +108,14 @@ func (s *server) handlerUpdateAccount(original storage.Account, updates account.
 	return http.StatusOK, updated, nil
 }
 
+func (s *server) handlerDeleteAccount(id uint) (int, interface{}, error) {
+	err := s.storage.DeleteAccount(id)
+	if err != nil {
+		return http.StatusBadRequest, nil, errors.Wrapf(err, "deleting Account with id:%d from storage", id)
+	}
+	return http.StatusOK, nil, nil
+}
+
 func extractID(vars map[string]string) (uint, error) {
 	if vars == nil {
 		return 0, errors.New("nil vars map")
