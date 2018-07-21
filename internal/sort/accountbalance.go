042be17cbc2ac911e2f20b78f3ed534d5687fc29
Glyn Owen Hanmer <glynhanmer@gmail.com>
2018-07-21 20:44:48 +0200

    Add sort.BalanceAmountMagnitude function

--- internal/sort/accountbalance.go
+++ internal/sort/accountbalance.go
@@ -15,3 +15,18 @@ func BalanceAmount(abs []accountbalance.AccountBalance) {
 		return (abs)[i].Balance.Amount < (abs)[j].Balance.Amount
 	})
 }
+
+func BalanceAmountMagnitude(abs []accountbalance.AccountBalance) {
+	sort.Slice(abs, func(i, j int) bool {
+		absI := absolute((abs)[i].Balance.Amount)
+		absJ := absolute((abs)[j].Balance.Amount)
+		return absI < absJ
+	})
+}
+
+func absolute(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
